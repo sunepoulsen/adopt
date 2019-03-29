e428785962a2c1d5576f24cd01473117df6d784e
sunepoulsen <sunepoulsen@gmail.com>
2019-03-29 03:53:13 +0100

    Add basic Adopt TopComponents Example.

---
+++ adopt-examples/adopt-example-topcomponents/adopt-example-topcomponents-app/src/main/java/dk/sunepoulsen/adopt/examples/topcomponents/TopComponentsApplication.java
@@ -0,0 +1,33 @@
+package dk.sunepoulsen.adopt.examples.topcomponents;
+
+import dk.sunepoulsen.adopt.javafx.application.AdoptJavaFXApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Application class.
+ */
+public class TopComponentsApplication extends AdoptJavaFXApplication {
+    private static Logger log = LoggerFactory.getLogger( TopComponentsApplication.class );
+
+    /**
+     * Main function.
+     *
+     * @param args Arguments from the command line.
+     */
+    public static void main( String[] args ) {
+        Properties settings = new Properties();
+        try {
+            settings.load( TopComponentsApplication.class.getResourceAsStream( "/application.properties" ) );
+        }
+        catch( IOException ex ) {
+            log.error( "Unable to load resource /application.properties", ex );
+        }
+
+        log.info( "Starting {} version {}", settings.getProperty( "application.name", "unknown" ), settings.getProperty( "application.version", "unknown" ) );
+        launchApplication( args );
+    }
+}
